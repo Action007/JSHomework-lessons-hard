b422b473181108e916cbb1d18cb488fed2875138
Aqshin Baghirzade <aqshin.baghirzade@gmail.com>
2021-06-02 10:13:13 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+try {
+  var form = document.querySelector('#form');
+
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    console.log('submit');
+  });
+} catch(e) {
+  console.error('form error');
+}
