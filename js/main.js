35f0de19755b5c25579899b7c3574f697128f4b0
Aqshin Baghirzade <aqshin.baghirzade@gmail.com>
2021-06-09 01:07:17 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let money = 300;
+let income = 'музыка';
+let addExpenses = 'Интернет, Коммуналка, Курсы';
+let deposit = true;
+let mission = 1000;
+let period = '9';
+let budgetDay = 200;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period +' месяцев');
+console.log('Цель заработать ' + mission + ' долларов');
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+console.log(budgetDay);
