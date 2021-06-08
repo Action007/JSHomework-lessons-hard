8c4dce79d1fde59644c4f0741fb73cce2d21548c
Aqshin Baghirzade <aqshin.baghirzade@gmail.com>
2021-06-09 01:29:56 +0400

    lesson02+

--- js/main.js
+++ js/main.js
@@ -1,17 +1,13 @@
-let money = 300;
-let income = 'музыка';
-let addExpenses = 'Интернет, Коммуналка, Курсы';
-let deposit = true;
-let mission = 1000;
-let period = '9';
-let budgetDay = 200;
+'use strict';
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-console.log(addExpenses.length);
-console.log('Период равен ' + period +' месяцев');
-console.log('Цель заработать ' + mission + ' долларов');
-console.log(addExpenses.toLowerCase());
-console.log(addExpenses.split(', '));
-console.log(budgetDay);
+let num = 266219;
+let result = 1;
+let final;
+
+for(let x = 0; x < String(num).length; x++) {
+  result *= String(num)[x];
+}
+
+final = result ** 3;
+
+console.log(String(final).slice(0, 2));
