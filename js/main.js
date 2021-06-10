a3bd4b5cd27b827dd49a69c0b06f253e7f29484f
Aqshin Baghirzade <aqshin.baghirzade@gmail.com>
2021-06-10 23:55:24 +0400

    lesson06+

--- js/main.js
+++ js/main.js
@@ -1,15 +1,41 @@
 'use strict';
 
-let myFunc = (arg) => {
-  if (typeof arg === 'string') {
-    if (arg.length > 30) {
-      console.log(arg.substring(0, 30) + "...");
-    } else {
-      console.log(arg);
+let isNumber = function (n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+let game = function () {
+
+  let num = 77;
+  let myFunc = () => {
+    for (let x = 9; x >= 0; x--) {
+      let quess = prompt('Угадай число от 1 до 100');
+      if (quess === null) {
+        return alert('Прощай!');
+      }
+      if (!isNumber(quess)) {
+        alert(`Введи число, загаданное число меньше, осталось попыток: ${x}`);
+      } else if (+quess === num) {
+        alert('Поздравляю, Вы угадали!!!');
+        let finalQuess = confirm('Хотели бы сыграть еще?');
+        if (finalQuess === true) {
+          myFunc();
+        }
+        return;
+      } else if (+quess > num) {
+        alert(`Загаданное число меньше, осталось попыток: ${x}`);
+      } else if (+quess < num) {
+        alert(`Загаданное число больше, осталось попыток: ${x}`);
+      }
     }
-  } else {
-    console.log('это не строка!');
+  };
+
+  myFunc();
+
+  let lastQuess = confirm('Попытки закончились, хотите сыграть еще ?');
+  if (lastQuess === true) {
+    myFunc();
   }
 };
 
-myFunc(prompt('Введите строку').trim());
+game();
