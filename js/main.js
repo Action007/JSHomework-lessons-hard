e20e2248239258e5ee125f21c73ca72d48e8c215
Aqshin Baghirzade <aqshin.baghirzade@gmail.com>
2021-06-10 17:50:13 +0400

    added lesson03+

--- js/main.js
+++ js/main.js
@@ -1,13 +1,39 @@
 'use strict';
 
-let num = 266219;
-let result = 1;
-let final;
+let lang = prompt('Введите язык');
+let namePerson = prompt('Введите имя');
+let final1;
+let final2;
 
-for(let x = 0; x < String(num).length; x++) {
-  result *= String(num)[x];
+// Вариант a
+if (lang === 'ru') {
+  console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+} else if (lang === 'en') {
+  console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+} else {
+  console.log('введите язык!');
 }
 
-final = result ** 3;
+// Вариант b
+switch (lang) {
+  case 'ru':
+    console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+    break;
+  case 'en':
+    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+    break;
+  default:
+    console.log('введите язык!');
+}
 
-console.log(String(final).slice(0, 2));
+// Вариант c
+const myObj = {
+  'ru': ['Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье'],
+  'en': ['Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday']
+};
+
+final1 = lang === 'ru' ? myObj.ru : lang === 'en' ? myObj.en : 'введите en или ru';
+final2 = namePerson === 'Артем' ? 'директор' : namePerson === 'Максим' ? 'преподаватель' : 'студент';
+
+console.log(final1);
+console.log(final2);
