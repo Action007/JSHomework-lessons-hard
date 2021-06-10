7dbc548636cd60e2dc575f4a0d5f44ede0fde34c
Aqshin Baghirzade <aqshin.baghirzade@gmail.com>
2021-06-11 01:03:18 +0400

    editet lesson04+

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 'use strict';
 
+let num = 242523333333234,
+  str = '        asdsadsdasd        ',
+  strLong = 'ooooooooooooooooooooooooooooooooooooooooooooooooooooo';
+
 let myFunc = (arg) => {
   if (typeof arg === 'string') {
     if (arg.length > 30) {
-      console.log(arg.substring(0, 30) + "...");
+      console.log(arg.substring(0, 29).trim() + "...");
     } else {
-      console.log(arg);
+      console.log(arg.trim());
     }
   } else {
     console.log('это не строка!');
   }
 };
 
-myFunc(prompt('Введите строку').trim());
+myFunc(strLong);
