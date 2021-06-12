6a3a7be779ac760484da39ee759bc543f48b264d
Aqshin Baghirzade <aqshin.baghirzade@gmail.com>
2021-06-12 22:50:44 +0400

    lesson05+

--- js/main.js
+++ js/main.js
@@ -1,15 +1,37 @@
 'use strict';
 
-let myFunc = (arg) => {
-  if (typeof arg === 'string') {
-    if (arg.length > 30) {
-      console.log(arg.substring(0, 30) + "...");
-    } else {
-      console.log(arg);
+let start = 0,
+  finish = 100;
+
+let arr = [
+  '855',
+  '4256',
+  '2574',
+  '2434',
+  '3464',
+  '3524',
+  '474'
+];
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i].charAt(0) === '2' || arr[i].charAt(0) === '4') {
+    console.log(arr[i]);
+  }
+}
+
+let prime = (num) => {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
     }
-  } else {
-    console.log('это не строка!');
+  }
+  if (num > 1) {
+    return true;
   }
 };
 
-myFunc(prompt('Введите строку').trim());
+for (let i = start; i <= finish; i++) {
+  if (prime(i)) {
+    console.log(`Число ${i} простое и его делители 1 и ${i}`);
+  }
+}
