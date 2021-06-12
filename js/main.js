9e662395d1f23c6bb3b489f40dce314b28027df3
Aqshin Baghirzade <aqshin.baghirzade@gmail.com>
2021-06-12 23:09:19 +0400

    lesson07+

--- js/main.js
+++ js/main.js
@@ -1,69 +1,23 @@
 'use strict';
 
-let isNumber = function (n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-};
+let arr,
+  week = [
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday',
+    'sunday',
+  ];
 
-let game = function () {
-
-  let num = Math.floor(Math.random() * 100);
-  let x = 10;
-  let myFunc = () => {
-    let quess = prompt('Угадай число от 1 до 100');
-    if (quess === null) {
-      return alert('Прощай!');
+  for (let i = 0; i < week.length; i++) {
+    if (i < 5) {
+      document.write(`${week[i]}</br>`);
+    } else if (i === 5) {
+      document.write(`<i>${week[i]}</i></br>`);
     }
-    if (!isNumber(quess)) {
-      --x;
-      if (x === 0) {
-        let lastQuess = confirm('Попытки закончились, хотите сыграть еще ?');
-        if (lastQuess === true) {
-          game();
-        } else {
-          return alert('Прощай!');
-        }
-      } else {
-        alert(`Введи число, осталось попыток: ${x}`);
-        myFunc();
-      }
-    } else if (+quess === num) {
-      alert('Поздравляю, Вы угадали!!!');
-      let finalQuess = confirm('Хотели бы сыграть еще?');
-      if (finalQuess === true) {
-        game();
-      } else {
-        return alert('Прощай!');
-      }
-    } else if (+quess > num) {
-      --x;
-      if (x === 0) {
-        let lastQuess = confirm('Попытки закончились, хотите сыграть еще ?');
-        if (lastQuess === true) {
-          game();
-        } else {
-          return alert('Прощай!');
-        }
-      } else if (x !== 0) {
-        alert(`Загаданное число меньше, осталось попыток: ${x}`);
-        myFunc();
-      }
-    } else if (+quess < num) {
-      --x;
-      if (x === 0) {
-        let lastQuess = confirm('Попытки закончились, хотите сыграть еще ?');
-        if (lastQuess === true) {
-          game();
-        } else {
-          return alert('Прощай!');
-        }
-      } else if (x !== 0) {
-        alert(`Загаданное число больше, осталось попыток: ${x}`);
-        myFunc();
-      }
+    else {
+      document.write(`<strong>${week[i]}</strong></br>`);
     }
-  };
-
-  myFunc();
-};
-
-game();
+}
