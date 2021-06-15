aa67528c9e2c779251e73bd6b38b52592a90d419
Aqshin Baghirzade <aqshin.baghirzade@gmail.com>
2021-06-16 00:49:46 +0400

    lesson09+

--- js/main.js
+++ js/main.js
@@ -1,23 +1,53 @@
 'use strict';
 
-let arr,
-  week = [
-    'monday',
-    'tuesday',
-    'wednesday',
-    'thursday',
-    'friday',
-    'saturday',
-    'sunday',
-  ];
-
-  for (let i = 0; i < week.length; i++) {
-    if (i < 5) {
-      document.write(`${week[i]}</br>`);
-    } else if (i === 5) {
-      document.write(`<i>${week[i]}</i></br>`);
-    }
-    else {
-      document.write(`<strong>${week[i]}</strong></br>`);
-    }
+function firstTime(value) {
+  if (value < 10) {
+    value = '0' + value;
+  }
+  return value;
 }
+
+function dateTime() {
+  let currentDatetime = new Date();
+  let day = firstTime(currentDatetime.getDate());
+  let month = firstTime(currentDatetime.getMonth() + 1);
+  let year = currentDatetime.getFullYear();
+  let hours = firstTime(currentDatetime.getHours());
+  let minutes = firstTime(currentDatetime.getMinutes());
+  let seconds = firstTime(currentDatetime.getSeconds());
+
+  return day + "." + month + "." + year + " " + hours + ":" + minutes + ":" + seconds;
+}
+
+let lastTime = function (number, titles) {
+  let cases = [2, 0, 1, 1, 1, 2];
+  if (number !== 0) {
+    return titles[
+      (number % 100 > 4 && number % 100 < 20) ?
+      2 :
+      cases[(number % 10 < 5) ? number % 10 : 5]
+    ];
+  } else {
+    return "00 часов";
+  }
+};
+
+function time2() {
+  let currentDatetime = new Date();
+  let day = currentDatetime.getDate();
+  let year = currentDatetime.getFullYear();
+  let hours = lastTime(currentDatetime.getHours(), ['час', 'часа', 'часов']);
+  let minutes = currentDatetime.getMinutes();
+  let seconds = currentDatetime.getSeconds();
+
+  return "Сегодня Вторник, " + day + " июня " + year + " года, " + hours + " " +
+    minutes + " минут " + seconds + " секунды";
+}
+
+setInterval(function () {
+  document.getElementById('time').innerHTML = time2();
+}, 1000);
+
+setInterval(function () {
+  document.getElementById('time2').innerHTML = dateTime();
+}, 1000);
