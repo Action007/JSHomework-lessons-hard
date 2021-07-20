608b5be09738848b330daa33f468c01f47d14536
Aqshin Baghirzade <aqshin.baghirzade@gmail.com>
2021-07-21 00:40:07 +0400

    bonus1

--- js/main.js
+++ js/main.js
@@ -1,13 +1,64 @@
 'use strict';
 
-let num = 266219;
-let result = 1;
-let final;
+const h1 = document.querySelector('.h1'),
+  signUp = document.querySelector('.sign-up'),
+  login = document.querySelector('.login'),
+  list = document.querySelector('.list'),
+  span = document.querySelector('.span'),
+  input = document.querySelector('input');
 
-for(let x = 0; x < String(num).length; x++) {
-  result *= String(num)[x];
-}
+let data = [];
+data = JSON.parse(localStorage.getItem('data'));
 
-final = result ** 3;
+const time = () => {
+  let currentDatetime = new Date(),
+    day = currentDatetime.getDate(),
+    year = currentDatetime.getFullYear(),
+    newDate = new Date().toTimeString().replace(/ .*/, '');
 
-console.log(String(final).slice(0, 2));
+  return day + " июля " + year + " г., " + newDate;
+};
+
+const test = (userName, log, password) => {
+  let arrName = userName.split(' ');
+  let i = 0;
+  data.push({
+    firstName: arrName[0],
+    lastName: arrName[1],
+    login: log,
+    password: password,
+    regDate: time()
+  });
+
+  localStorage.setItem('data', JSON.stringify(data));
+
+  let li = document.createElement('li');
+  let logOut = document.createElement('button');
+
+  li.textContent = `Имя: ${data[i].firstName}, Фамилия: ${data[i].lastName},
+  зарегистрирован: ${data[i].regDate}`;
+
+  logOut.textContent = 'X';
+  list.append(li);
+  li.append(logOut);
+  span.textContent = data[i].firstName;
+  i += 1;
+};
+
+const logIn = () => {
+  let userName = prompt('Введите через пробел имя и фамилию пользователя'),
+    log = prompt('Введите Логин'),
+    password = prompt('Введите Пароль'),
+    result = userName.match(
+      /^[a-zA-Zа-яА-Я]+ [a-zA-Zа-яА-Я]+$/
+    );
+
+  if (result) {
+    test(userName, log, password);
+  }
+  console.log(data);
+};
+
+signUp.addEventListener('click', () => {
+  logIn();
+});
