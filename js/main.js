403dcde79c8d05f57007bb09714d1f2c13f3649f
Aqshin Baghirzade <aqshin.baghirzade@gmail.com>
2021-07-25 03:33:37 +0400

    lesson17+ part1

--- js/main.js
+++ js/main.js
@@ -1,174 +1,22 @@
-window.addEventListener('DOMContentLoaded', function () {
-  'use strict';
+'use strict';
 
-  // timer
-  const countTimer = (deadline) => {
-    const timerHours = document.getElementById('timer-hours'),
-      timerMinutes = document.getElementById('timer-minutes'),
-      timerSeconds = document.getElementById('timer-seconds'),
-      span = document.querySelectorAll('.span');
+let input = document.querySelector('input'),
+  p = document.querySelector('p');
 
+  console.log(input);
+  console.log(p);
 
-    const getTimeRemaining = () => {
-      const dateStop = new Date(deadline).getTime(),
-        dateNow = new Date().getTime(),
-        timeRemaining = (dateStop - dateNow) / 1000,
-        seconds = Math.floor(timeRemaining % 60),
-        minutes = Math.floor((timeRemaining / 60) % 60),
-        hours = Math.floor(timeRemaining / 60 / 60);
-      return {
-        timeRemaining,
-        hours,
-        minutes,
-        seconds
-      };
-    };
+const setFunc = () => {
+  p.textContent = input.value;
+};
 
-    const updateClock = () => {
-      const timer = getTimeRemaining();
-      timerHours.textContent = timer.hours;
-      timerMinutes.textContent = timer.minutes;
-      timerSeconds.textContent = timer.seconds;
-      if (timer.timeRemaining > 0) {
-        if (timerHours.textContent < 10) {
-          timerHours.textContent = '0' + timerHours.textContent;
-        }
-        if (timerMinutes.textContent < 10) {
-          timerMinutes.textContent = '0' + timerMinutes.textContent;
-        }
-        if (timerSeconds.textContent < 10) {
-          timerSeconds.textContent = '0' + timerSeconds.textContent;
-        }
-      } else {
-        clearInterval(SetTimer);
-        timerHours.textContent = '00';
-        timerMinutes.textContent = '00';
-        timerSeconds.textContent = '00';
-        timerHours.style.color = 'red';
-        timerMinutes.style.color = 'red';
-        timerSeconds.style.color = 'red';
-        span.forEach(i => {
-          i.style.color = 'red';
-        });
-      }
-    };
+const timeOut = () => {
+  let time;
+  return () => {
+    clearTimeout(time);
 
-    const SetTimer = setInterval(updateClock, 1000);
-    updateClock();
+    time = setTimeout(setFunc, 300);
   };
+};
 
-  countTimer('11 july 2021');
-
-  // menu
-  const toggleMenu = () => {
-    const body = document.querySelector('body'),
-      menu = document.querySelector('menu');
-
-    let actionMenu = () => { 
-      menu.classList.toggle('active-menu');
-    };
-    body.addEventListener('click', (event) => {
-      let target = event.target;
-      if (target.classList.contains('close-btn')) {
-        actionMenu();
-      } else if (target.closest('li')) {
-        actionMenu();
-      } else if (target.closest('.menu')) {
-        actionMenu();
-      } else if (menu.classList.value === 'active-menu') {
-        target = target.closest('menu');
-        if (!target) {
-          actionMenu();
-        }
-      }
-    });
-  };
-  toggleMenu();
-
-  // popup
-  const togglePopup = () => {
-    const popup = document.querySelector('.popup'),
-      popupBtn = document.querySelectorAll('.popup-btn');
-
-    let fadeIn = (element) => {
-      element.style.display = 'block';
-      if (screen.width > 768) {
-        element.style.opacity = '0';
-        let tick = () => {
-          element.style.opacity = +element.style.opacity + 0.1;
-          if (+element.style.opacity < 1) {
-            setTimeout(tick, 16);
-          }
-        };
-        tick();
-      }
-    };
-
-    let fadeOut = (element) => {
-      let tick = () => {
-        if (+element.style.opacity !== 0) {
-          element.style.opacity = +element.style.opacity - 0.1;
-          setTimeout(tick, 16);
-        } else {
-          element.style.display = 'none';
-        }
-      };
-      tick();
-    };
-
-    popupBtn.forEach(item => {
-      item.addEventListener("click", () => {
-        fadeIn(popup);
-      });
-    });
-
-    popup.addEventListener('click', (event) => {
-      let target = event.target;
-
-      if (target.classList.contains('popup-close')) {
-        fadeOut(popup);
-      } else {
-        target = target.closest('.popup-content');
-        if (!target) {
-          fadeOut(popup);
-        }
-      }
-    });
-  };
-
-  togglePopup();
-
-  // tabs
-
-  const tabs = () => {
-    const tabHeader = document.querySelector('.service-header'),
-      tab = tabHeader.querySelectorAll('.service-header-tab'),
-      tabContent = document.querySelectorAll('.service-tab');
-
-    const toggleTab = (index) => {
-      for (let i = 0; i < tabContent.length; i++) {
-        if (index === i) {
-          tab[i].classList.add('active');
-          tabContent[i].classList.remove('d-none');
-        } else {
-          tab[i].classList.remove('active');
-          tabContent[i].classList.add('d-none');
-        }
-      }
-    };
-    tabHeader.addEventListener('click', (event) => {
-      let target = event.target;
-      target = target.closest('.service-header-tab');
-
-      if (target) {
-        tab.forEach((item, index) => {
-          if (item === target) {
-            toggleTab(index);
-          }
-        });
-        return;
-      }
-    });
-  };
-  tabs();
-});
+input.addEventListener('keyup', timeOut());
