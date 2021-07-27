a1388753d8ffcd0584caa1f44c01e4faccbb4ac3
Aqshin Baghirzade <aqshin.baghirzade@gmail.com>
2021-07-27 06:07:03 +0400

    lesson18+

--- js/main.js
+++ js/main.js
@@ -174,13 +174,29 @@ window.addEventListener('DOMContentLoaded', function () {
 
   // Smooth scrolling
   const scroll = () => {
-    const scrollBtn = document.querySelector('a[href="#service-block"]');
-    const scrollBlock = document.getElementById('service-block');
+    const scrollBtn = document.querySelector('a[href="#service-block"]'),
+      scrollBlock = document.getElementById('service-block'),
+      scrollMenu = document.querySelectorAll('menu>ul>li>a');
+
     scrollBtn.addEventListener('click', (event) => {
       event.preventDefault();
       scrollBlock.scrollIntoView({
         behavior: "smooth",
         block: 'start'
+      });
+    });
+
+    scrollMenu.forEach(item => {
+      item.addEventListener('click', (event) => {
+        let link = item.getAttribute('href').substring(1),
+          scrollBlock = document.getElementById(link);
+
+        event.preventDefault();
+
+        scrollBlock.scrollIntoView({
+          behavior: "smooth",
+          block: 'start'
+        });
       });
     });
   };
